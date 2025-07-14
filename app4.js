f4a61e0630b3ea9df95a857ffa91652d7dc13d3d
liviabbxavier <liviabrocardo43@gmail.com>
2025-07-14 11:27:39 -0300

    Add files via upload

---
+++ app4.js
@@ -0,0 +1,28 @@
+alert('Bem vindo ao jogo do número secreto');
+let numeroSecreto = 15;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número.');
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert('O númer secreto é menor que ' + chute);
+        } else {
+            alert('O número secreto é maior que ' + chute);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
+    }
+}
+
+let palavraTentativa = chute > 1 ? 'tentativas' : 'tentativa'
+if (chute > 1) {
+    alert('Parabéns, você descobriu o número secreto ' + numeroSecreto + ' com ' + tentativas + ' tentativas.');
+} else {
+    alert('Parabéns, você descobriu o número secreto ' + numeroSecreto + ' com ' + tentativas + ' tentativa.');
+}
