151d9da6e8fab0ddd801f03adcb231db8ea8f228
liviabbxavier <liviabrocardo43@gmail.com>
2025-07-15 11:53:22 -0300

    Add files via upload

---
+++ Desafios/desafio4.js
@@ -0,0 +1,32 @@
+console.log('Seja bem vindo!');
+let nome = 'Lívia';
+console.log(`Olá, ${nome}`);
+let nome2 = 'Lívia';
+alert (`Olá, ${nome2}`);
+nome2 = prompt('Qual a linguagem de programação que você mais gosta?');
+let resposta = 'Java';
+
+let valor1 = 7;
+let valor2 = 29;
+let resultado = 36;
+console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`);
+
+let valor3 = 7;
+let valor4 = 29;
+let resultado1 = 22;
+console.log(`A diferença entre ${valor3} e ${valor4} é igual a ${resultado1}.`);
+
+let idade = prompt('Digite sua idade.');
+if (idadeDoUsuario >= 18) {
+    console.log('Você é maior de idade!');
+} else {
+    console.log('Você é menor de idade!');
+}
+
+let numero = prompt('Digite um número negativo ou positivo.');
+if (numero <= -1) {
+    console.log('Este número é negativo!');
+} else {
+    console.log('Este número é positivo!');
+}
+
