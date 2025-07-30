15a77c9994e60e61e032f8cd56e06c7f40a60dea
liviabbxavier <liviabrocardo43@gmail.com>
2025-07-30 11:45:37 -0300

    Add files via upload

---
+++ Java/Desafios/IntelliJ/src/Desafio3/Tabuada.java
@@ -0,0 +1,11 @@
+package Desafio3;
+
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        Scanner tabuada = new Scanner(System.in);
+        System.out.println("Digite um número e descubra a tabuada");
+        int numero = tabuada.nextInt();
+    }
+}
