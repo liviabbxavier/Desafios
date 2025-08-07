4e03849c2bbcd196b1dca5a18721fc7db16b6655
liviabbxavier <liviabrocardo43@gmail.com>
2025-08-07 11:34:39 -0300

    Add files via upload

---
+++ Java/Desafios/IntelliJ/src/OrientacaoAObjetos/orientacaoobjeto/filme/Filme.java
@@ -0,0 +1,13 @@
+package br.com.alura.orientacaoobjeto.filme;
+
+public class Filme extends Titulo{
+   private String diretor;
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+}
---
+++ Java/Desafios/IntelliJ/src/OrientacaoAObjetos/orientacaoobjeto/filme/Serie.java
@@ -0,0 +1,45 @@
+package br.com.alura.orientacaoobjeto.filme;
+
+public class Serie extends Titulo{
+    private int temporadas;
+    private int totalDeEpisodios;
+    private boolean ativa;
+    private int minutosPorEpisodios;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getTotalDeEpisodios() {
+        return totalDeEpisodios;
+    }
+
+    public void setTotalDeEpisodios(int totalDeEpisodios) {
+        this.totalDeEpisodios = totalDeEpisodios;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getMinutosPorEpisodios() {
+        return minutosPorEpisodios;
+    }
+
+    public void setMinutosPorEpisodios(int minutosPorEpisodios) {
+        this.minutosPorEpisodios = minutosPorEpisodios;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * totalDeEpisodios * minutosPorEpisodios;
+    }
+}
