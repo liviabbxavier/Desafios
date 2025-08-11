f29bd9d8b3d8c3758d5bff60a445a411e763bcae
liviabbxavier <liviabrocardo43@gmail.com>
2025-08-11 08:07:46 -0300

    Add files via upload

--- Java/Desafios/IntelliJ/src/OrientacaoAObjetos/Modulo3/Carro/Carro.java
+++ Java/Desafios/IntelliJ/src/OrientacaoAObjetos/Modulo3/Carro/Carro.java
@@ -3,9 +3,12 @@ package Modulo3.Carro;
 public class Carro {
     public static void main(String[] args) {
         ModeloCarro meuCarro = new ModeloCarro();
-        meuCarro.setNome("Ferrari");
-        meuCarro.setModelo("12Cilindri Super");
-        meuCarro.especificacoes();
-        meuCarro.setPrecoMaisAlto(1000000);
+        meuCarro.setNome("Clio");
+        meuCarro.setModelo("duas portas");
+        meuCarro.setAno1(28900);
+        meuCarro.setAno2(27900);
+        meuCarro.setAno3(25900);
+        meuCarro.setPrecoMaisAlto(28900);
+        meuCarro.precoMedio();
     }
 }
--- Java/Desafios/IntelliJ/src/OrientacaoAObjetos/Modulo3/Carro/ModeloCarro.java
+++ Java/Desafios/IntelliJ/src/OrientacaoAObjetos/Modulo3/Carro/ModeloCarro.java
@@ -1,9 +1,12 @@
 package Modulo3.Carro;
 
-public class ModeloCarro extends Carro{
+public class ModeloCarro extends Carro {
     private String modelo;
     private String nome;
     private int precoMaisAlto;
+    private int ano1;
+    private int ano2;
+    private int ano3;
 
     public String getModelo() {
         return modelo;
@@ -29,9 +32,42 @@ public class ModeloCarro extends Carro{
         this.precoMaisAlto = precoMaisAlto;
     }
 
-    public void especificacoes() {
+    public double getAno1() {
+        return ano1;
+    }
+
+    public void setAno1(int ano1) {
+        this.ano1 = ano1;
+    }
+
+    public double getAno2() {
+        return ano2;
+    }
+
+    public void setAno2(int ano2) {
+        this.ano2 = ano2;
+    }
+
+    public double getAno3() {
+        return ano3;
+    }
+
+    public void setAno3(int ano3) {
+        this.ano3 = ano3;
+    }
+
+    public void precoMedio() {
+        double precoMedio = (ano1 + ano2 + ano3) / 3;
+        double menorPreco = Math.min(ano1, Math.min(ano2, ano3));
+        double maiorPreco = Math.max(ano1, Math.max(ano2, ano3));
+        System.out.println(precoMedio);
+
         System.out.println("Nome do carro: " + nome);
         System.out.println("Modelo: " + modelo);
-        System.out.println("Preço: " + precoMaisAlto);
+        System.out.println("Preço 2012: " + "R$ " + ano1);
+        System.out.println("Preço 2013: " + "R$ " + ano2);
+        System.out.println("Preço 2014: " + "R$ " + ano3);
+        System.out.println("Menor preço: " + "R$ " + menorPreco);
+        System.out.println("Maior preço: " + "R$ " + maiorPreco);
     }
 }
